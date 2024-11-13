c72fcca27b921d471bbebe455de15ad4d74b658f
82orez <82orez@naver.com>
2024-11-13 17:28:05 +0900

    meta data

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -1,6 +1,16 @@
 import MovieCardClient from "@/app/movies/[id]/movieCardClient";
 import { getMovie, prismaGetMovie } from "@/server-actions/actions";
 
+export async function generateMetadata({ params }) {
+  const movie = await getMovie(params.id);
+
+  return {
+    title: movie.title,
+    description: movie.overview,
+    openGraph: { images: [movie.image_url] },
+  };
+}
+
 export default async function MovieDetailPage({ params }) {
   const movie = await prismaGetMovie(params.id);
   return <MovieCardClient movie={movie} />;
